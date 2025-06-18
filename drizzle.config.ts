3c764dc51c0efa08d07ae79b3a28afc63738128d
semideus-dev <semideus.dev@gmail.com>
2025-06-18 19:18:34 +0530

    v0.1.1 - auth (db setup)

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+import env from "@/lib/env";
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./src/lib/db/schema.ts",
+  out: "./drizzle",
+  dialect: "postgresql",
+  dbCredentials: { url: env.databaseUrl! },
+});
